03c861ee32b2a7e45a684d34a735e7100ba6026c
danding <1291248710@qq.com>
2017-04-03 13:44:36 +0800

    xx

--- web/src/containers/Workspace.jsx
+++ web/src/containers/Workspace.jsx
@@ -19,64 +19,64 @@ class Workspace extends Component {
 
 
 
-        this.props.dispatch(importComponent(item)).then((payload) => {
-            alert('callback=========importComponent type='+payload.type);
-            //always got null
-            console.log(payload);
-
-
-            var reportStr='description about component item=======  \r\n';
-            reportStr+='template url=======\r\n'+item.template.text+'\r\n';
-            reportStr+='dependencies=====\r\n';
-            if(item.dependency!==undefined&&item.dependency!==null)
-            {
-                item.dependencies.map(function(dependency,i){
-                    reportStr+=dependency.toString();
-                });
-            }
-            if(item.template.metadata!==undefined&&item.template.metadata!==null)
-            {
-                reportStr+='metadata url=========\r\n';
-                reportStr+=item.template.metadata;
-            }
-
-            if(item.imports!==undefined&&item.imports!==null)
-            {
-                reportStr+='\r\nimports==========\r\n';
-                reportStr+=item.imports;
-            }
-            console.log(reportStr);
-
-
-            //获取模板内容,并引入依赖
-            fetchTemplateAndImportDependencies(
-                item.dependencies,
-                item.template.text,
-                item.template.metadata,
-                this.props.rootPath,
-                this.props.npmRegistry,
-            ).then(({text, metadata}) => {
-                const {decoDoc} = this.props
-
-                if (! decoDoc) {
-                    return
-                }
-                //metadata含有liveValues的json形式
-                var metadataStr='metadata=============\r\n';
-                for(var field in metadata)
-                    metadataStr+=field+'=\r\n'+metadata[field];
-                console.log(metadataStr);
-
-                //插入组件template
-                this.props.dispatch(insertTemplate(
-                    decoDoc,
-                    text,
-                    metadata,
-                    item.imports,
-                    _.get(item, 'inspector.group')
-                ))
-            })
-        })
+        // this.props.dispatch(importComponent(item)).then((payload) => {
+        //     alert('callback=========importComponent type='+payload.type);
+        //     //always got null
+        //     console.log(payload);
+        //
+        //
+        //     var reportStr='description about component item=======  \r\n';
+        //     reportStr+='template url=======\r\n'+item.template.text+'\r\n';
+        //     reportStr+='dependencies=====\r\n';
+        //     if(item.dependency!==undefined&&item.dependency!==null)
+        //     {
+        //         item.dependencies.map(function(dependency,i){
+        //             reportStr+=dependency.toString();
+        //         });
+        //     }
+        //     if(item.template.metadata!==undefined&&item.template.metadata!==null)
+        //     {
+        //         reportStr+='metadata url=========\r\n';
+        //         reportStr+=item.template.metadata;
+        //     }
+        //
+        //     if(item.imports!==undefined&&item.imports!==null)
+        //     {
+        //         reportStr+='\r\nimports==========\r\n';
+        //         reportStr+=item.imports;
+        //     }
+        //     console.log(reportStr);
+        //
+        //
+        //     //获取模板内容,并引入依赖
+        //     fetchTemplateAndImportDependencies(
+        //         item.dependencies,
+        //         item.template.text,
+        //         item.template.metadata,
+        //         this.props.rootPath,
+        //         this.props.npmRegistry,
+        //     ).then(({text, metadata}) => {
+        //         const {decoDoc} = this.props
+        //
+        //         if (! decoDoc) {
+        //             return
+        //         }
+        //         //metadata含有liveValues的json形式
+        //         var metadataStr='metadata=============\r\n';
+        //         for(var field in metadata)
+        //             metadataStr+=field+'=\r\n'+metadata[field];
+        //         console.log(metadataStr);
+        //
+        //         //插入组件template
+        //         this.props.dispatch(insertTemplate(
+        //             decoDoc,
+        //             text,
+        //             metadata,
+        //             item.imports,
+        //             _.get(item, 'inspector.group')
+        //         ))
+        //     })
+        //})
     }
 
 
